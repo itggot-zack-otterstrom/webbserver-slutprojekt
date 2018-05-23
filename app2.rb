b9fbf26ff43ddab74a8f36c26db8908aa35df769
itggot-zack-otterstrom <zack.otterstrom@itggot.se>
2018-05-23 16:01:38 +0200

    Färdig med moduler

--- app2.rb
+++ app2.rb
@@ -14,7 +14,7 @@ end
 get '/start' do
     if session[:user_id] != nil
         db = db_connect()
-        products = db.execute("SELECT * FROM products")
+        products = get_all_products()
         erb(:start, locals: {products: products})
     else
         redirect('/error_login')
@@ -27,8 +27,7 @@ end
 
 get '/shoppa/?' do
     if session[:user_id] != nil
-        db = db_connect()
-        products = db.execute("SELECT * FROM products")
+        products = get_all_products()
         erb(:shoppa, locals: {products: products})        
     else
         redirect('/error_login')
@@ -41,8 +40,7 @@ end
 
 get '/all_designers' do
     if session[:user_id] != nil
-        db = db_connect()
-        designer = db.execute("SELECT * FROM designers")
+        designer = get_all_designers()
         erb(:all_designers, locals: { designers:designer})
     else
         redirect('/error_login')
@@ -51,10 +49,9 @@ end
 
 get '/designers/:id/?' do
     if session[:user_id] != nil
-        db = db_connect()
-        designer = db.execute("SELECT * FROM designers WHERE id=?",[ params[:id]])
-        designer = designer[0]
-        product = db.execute("SELECT * FROM products WHERE id IN (SELECT product_id FROM 'product_designer_relation' WHERE designer_id=?)", [ params[:id]])
+        id = [ params[:id]]
+        designer = get_designer(id)
+        product = get_product_with_designer_id(id)
         erb(:designers, locals: { designers:designer, products:product })
     else
         redirect('/error_login')
@@ -63,10 +60,9 @@ end
 
 get '/product/:id/?' do
     if session[:user_id] != nil
-        db = db_connect()
-        product = db.execute("SELECT * FROM products WHERE id=?",[ params[:id]])
-        product = product[0]
-        designer = db.execute("SELECT * FROM designers WHERE id IN (SELECT designer_id FROM 'product_designer_relation' WHERE product_id=?)", [ params[:id]])
+        id = [ params[:id]]
+        product = get_product(id)
+        designer = get_designer_with_product_id(id)
         erb(:product, locals: { products:product, designer:designer})
     else
         redirect('/error_login')
@@ -103,13 +99,13 @@ post '/login' do
     name = params[:name]
     password = params[:password]
     db = db_connect()
-    real_password = db.execute("SELECT password FROM users WHERE name=?", name)
+    real_password = get_user_password(name)
     if real_password != [] && BCrypt::Password.new(real_password[0][0]) == password
-        session[:user_id] = db.execute("SELECT id FROM users WHERE name=?", name)[0][0]
-        session[:user_name] = db.execute("SELECT name FROM users WHERE id=?", session[:user_id])[0][0]
+        session[:user_id] = get_user_id_with_username(name)
+        user_id = session[:user_id]
+        session[:user_name] = get_username_with_user_id(user_id)
         redirect('/start') 
     else
-        session[:failure] = "Login failed"
         redirect('/')
     end
 end
@@ -124,9 +120,8 @@ post '/change_name' do
     old_name = params[:old_name]
     new_name = params[:new_name]
     if old_name == session[:user_name]
-        db = db_connect()
-        db.execute("UPDATE users SET name = '#{new_name}' WHERE id=?", session[:user_id])
-        session[:user_name] = db.execute("SELECT name FROM users WHERE id=?", session[:user_id])[0][0]
+        new_name = params[:new_name]
+        change_name(new_name)
         redirect('/start')
     else
         redirect('/konto')
--- model/model.rb
+++ model/model.rb
@@ -18,4 +18,57 @@ module DesicDB
         db.execute("INSERT INTO users (name, password) VALUES (?,?)", [new_name, hashed_password])
         redirect('/')
     end
+
+    def get_all_products
+        db = db_connect()
+        products = db.execute("SELECT * FROM products")
+    end
+
+    def get_all_designers
+        db = db_connect()
+        designer = db.execute("SELECT * FROM designers")
+    end
+
+    def get_designer(id)
+        db = db_connect()
+        designer = db.execute("SELECT * FROM designers WHERE id=?", id)
+        designer = designer[0]
+    end
+
+    def get_product(id)
+        db = db_connect()
+        product = db.execute("SELECT * FROM products WHERE id=?", id)
+        product = product[0]
+    end
+
+    def get_product_with_designer_id(id)
+        db = db_connect()
+        product = db.execute("SELECT * FROM products WHERE id IN (SELECT product_id FROM 'product_designer_relation' WHERE designer_id=?)", id)
+    end
+
+    def get_designer_with_product_id(id)
+        db = db_connect()
+        designer = db.execute("SELECT * FROM designers WHERE id IN (SELECT designer_id FROM 'product_designer_relation' WHERE product_id=?)", id)
+    end
+
+    def get_user_password(name)
+        db = db_connect()
+        real_password = db.execute("SELECT password FROM users WHERE name=?", name)
+    end
+
+    def get_user_id_with_username(name)
+        db = db_connect()
+        id = db.execute("SELECT id FROM users WHERE name=?", name)[0][0]
+    end
+
+    def get_username_with_user_id(id)
+        db = db_connect()
+        username = db.execute("SELECT name FROM users WHERE id=?", session[:user_id])[0][0]
+    end
+
+    def change_name(new_name)
+        db = db_connect()
+        db.execute("UPDATE users SET name = '#{new_name}' WHERE id=?", session[:user_id])
+        session[:user_name] = db.execute("SELECT name FROM users WHERE id=?", session[:user_id])[0][0]
+    end
 end
